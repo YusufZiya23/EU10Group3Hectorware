82e27f55ef2bfa20f77e388a1fdd2d63cf9be6d3
Magbule Bamja <magbule.bamja@gmail.com>
2023-03-15 21:15:39 +0100

    I created TC-02 feature,page,steps and tests passed

---
+++ src/test/java/com/cydeo/pages/OnlineStatusPage.java
@@ -0,0 +1,34 @@
+package com.cydeo.pages;
+
+import com.cydeo.utilites.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class OnlineStatusPage {
+
+
+
+    public OnlineStatusPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+
+
+    @FindBy(xpath = "//div[@class='avatardiv avatardiv-shown']")
+    public WebElement avatarButton;
+
+    @FindBy(xpath = "//div[@class='user-status-menu-item']/a")
+   public WebElement SetStatusLinkDropDown;
+
+    @FindBy(xpath = "//div[@class='set-status-modal']")
+    public WebElement OnlineStatusBox;
+
+
+
+
+
+
+
+
+}
