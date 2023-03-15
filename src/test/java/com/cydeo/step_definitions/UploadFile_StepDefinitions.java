189e0ae30577857354bae2e6ad438994ad21e390
YusufZiya <yusufziyagunes1@gmail.com>
2023-03-15 19:08:18 +0100

    Yusuf ziya day2, Added the feature file and step definition for upload file user story.

---
+++ src/test/java/com/cydeo/step_definitions/UploadFile_StepDefinitions.java
@@ -0,0 +1,47 @@
+package com.cydeo.step_definitions;
+
+import com.cydeo.pages.LoginPage;
+import com.cydeo.pages.UploadFilePage;
+import com.cydeo.utilites.Driver;
+import io.cucumber.java.en.*;
+import org.junit.Assert;
+
+public class UploadFile_StepDefinitions {
+
+    LoginPage loginPage = new LoginPage();
+    UploadFilePage uploadFilePage = new UploadFilePage();
+
+    @Given("user logged in to an account")
+    public void user_logged_in_to_an_account() {
+        Driver.getDriver().get("https://qa.hectorware.com/index.php/login?clear=1");
+        loginPage.username.sendKeys("Employee1");
+        loginPage.password.sendKeys("Employee123");
+        loginPage.LoginButton.click();
+    }
+    @Given("user should verify the plus button is displayed")
+    public void user_should_verify_the_plus_button_is_displayed() {
+        Assert.assertTrue(uploadFilePage.plusBtn.isDisplayed());
+    }
+    @Then("user should click to plus button")
+    public void user_should_click_to_plus_button() {
+        uploadFilePage.plusBtn.click();
+    }
+
+    @Then("user sees dropdown and confirms required texts are displayed")
+    public void userSeesDropdownAndConfirmsRequiredTextsAreDisplayed() {
+        Assert.assertTrue(uploadFilePage.uploadFileBtn.isDisplayed());
+        Assert.assertTrue(uploadFilePage.newFolderBtn.isDisplayed());
+        Assert.assertTrue(uploadFilePage.newTextDocBtn.isDisplayed());
+    }
+
+
+    @Given("user should select a file to upload by clicking to Upload file button")
+    public void userShouldSelectAFileToUploadByClickingToUploadFileButton() {
+        uploadFilePage.uploadFileBtn.sendKeys("C:\\Users\\DT User\\Desktop");
+    }
+
+    @Then("user verifies uploaded file is displayed")
+    public void userVerifiesUploadedFileIsDisplayed() {
+        Assert.assertTrue(uploadFilePage.file.isDisplayed());
+    }
+}
---
+++ src/test/resources/features/uploadFile.feature
@@ -0,0 +1,22 @@
+Feature: Upload file feature
+
+  User story : As a user, I'd like to be able to add a file so that I can share them with other app users.
+
+  Background: user is expected to be on the login page
+    Given user logged in to an account
+    Then user should click to plus button
+
+
+  Scenario: User should see the plus button right next to home button
+    Given user should verify the plus button is displayed
+
+  Scenario: User should see the following options after clicking to plus button: Upload file, New folder, New text document
+    Then user sees dropdown and confirms required texts are displayed
+
+  Scenario: User should click to upload file button and select a text, img or pdf file with a maximum capacity of 10 mb
+    Then user should select a file to upload by clicking to Upload file button
+
+  @yzg
+  Scenario: User can verify file is uploaded by confirming it’s displayed in the ‘’All files’’ page.
+    Given user should select a file to upload by clicking to Upload file button
+    Then user verifies uploaded file is displayed
