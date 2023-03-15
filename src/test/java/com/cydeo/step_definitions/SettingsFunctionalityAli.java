338ce4eb2a67a8e404119485490a99f784164220
Ali <yerligul.ali@gmail.com>
2023-03-15 17:16:55 +0100

    Hi Guys, unfortunatelly i can not get my snippets to add them as stepdefinitions. maybe my computer is to slow.So can anybody run this at his local?

---
+++ src/test/java/com/cydeo/step_definitions/SettingsFunctionalityAli.java
@@ -0,0 +1,11 @@
+package com.cydeo.step_definitions;
+
+import com.cydeo.utilites.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class SettingsFunctionalityAli {
+
+
+}
