1a34848b41cd7ef25b324b5464ffa542cce178ef
Seyma Avci <symvci35@gmail.com>
2023-03-17 21:33:21 +0100

    contacts function edited

--- src/test/java/com/cydeo/step_definitions/Contacts_StepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/Contacts_StepDefinitions.java
@@ -7,6 +7,8 @@ import com.cydeo.utilites.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.Keys;
 
 public class Contacts_StepDefinitions {
 
@@ -26,34 +28,51 @@ public class Contacts_StepDefinitions {
     @When("Navigate to Contacts module on the left side of the page")
     public void navigate_to_contacts_module_on_the_left_side_of_the_page() {
 
+        contactsPage.contactsButton.click();
 
     }
     @When("Click New contact button")
     public void click_new_contact_button() {
 
-
+        contactsPage.newContactButton.click();
 
     }
     @When("Enter {string} in to Name inbox")
     public void enter_in_to_name_inbox(String name) {
 
+        contactsPage.name.click();
+        contactsPage.name.sendKeys(Keys.DELETE + "Cedric Diggory");
 
 
     }
     @When("Enter {string} in to Company inbox")
     public void enter_in_to_company_inbox(String string) {
 
+        contactsPage.company.sendKeys("Amazon");
+
     }
     @When("Enter {string} in to Title inbox")
     public void enter_in_to_title_inbox(String string) {
+
+        contactsPage.title.sendKeys("Mr");
 
     }
     @When("Click any empty area on the page")
     public void click_any_empty_area_on_the_page() {
 
-    }
-    @Then("Verify new contact is shown in contact list")
-    public void verify_new_contact_is_shown_in_contact_list() {
+        contactsPage.emptyArea.click();
 
     }
+   @Then("Verify new contact is shown in contact list")
+    public void verify_new_contact_is_shown_in_contact_list() {
+
+       Assert.assertTrue(contactsPage.contact.isDisplayed());
+
+    }
+
+
+
+
+
+
 }
