b8a7913ab05634c47375ae74b25f86c46ae9b47a
Albert Shima <albertshima@gmail.com>
2023-03-17 10:34:37 +0100

    I added more scenarios for my user story BERTI

--- src/test/java/com/cydeo/pages/ProfileSettingsPageBerti.java
+++ src/test/java/com/cydeo/pages/ProfileSettingsPageBerti.java
@@ -1,6 +1,7 @@
 package com.cydeo.pages;
 
 import com.cydeo.utilites.Driver;
+import okhttp3.internal.http2.Settings;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
