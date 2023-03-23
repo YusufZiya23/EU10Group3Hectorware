3f7a6a7c42fdde1f0c3bedbebe5eaaf3b32edbfd
Ali <yerligul.ali@gmail.com>
2023-03-23 01:27:24 +0100

    some added new codes

--- src/test/java/com/cydeo/step_definitions/SettingsStepDefinitionsAli.java
+++ src/test/java/com/cydeo/step_definitions/SettingsStepDefinitionsAli.java
@@ -1,20 +1,32 @@
+
+
+
+
+
+
+
 package com.cydeo.step_definitions;
 
 import com.cydeo.pages.SettingsPageAli;
 import com.cydeo.utilites.ConfigurationReader;
 import com.cydeo.utilites.Driver;
+
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+
+
 
 public class SettingsStepDefinitionsAli {
-SettingsPageAli settings= new SettingsPageAli();
+    SettingsPageAli settings= new SettingsPageAli();
 
-    @When("User logs in with valid credentions")
+    @When("User logs in with valid credential")
     public void user_logs_in_with_valid_credentions() {
-Driver.getDriver().get("https://qa.hectorware.com/index.php/login");
-settings.username.sendKeys(ConfigurationReader.getProperty("username"));
-settings.password.sendKeys(ConfigurationReader.getProperty("password"));
-settings.LoginButton.click();
+        Driver.getDriver().get("https://qa.hectorware.com/index.php/login");
+
+        settings.username.sendKeys(ConfigurationReader.getProperty("username"));
+        settings.password.sendKeys(ConfigurationReader.getProperty("password"));
+        settings.LoginButton.click();
 
     }
 
@@ -34,5 +46,39 @@ settings.LoginButton.click();
     @Then("User Clicks on Settings button")
     public void userClicksOnSettingsButton() {
         settings.SettingsButton.click();
+    }
+
+
+    @When("User enters a valid full name Ali Yerligul")
+    public void userEnters() {
+
+        settings.fullName.clear();
+        settings.fullName.sendKeys("Ali Yerligul");
+
+    }
+
+    @Then("user sees Ali Yerligul after refreshing")
+    public void userSeesExpected()  {
+        Driver.getDriver().navigate().refresh();
+
+
+        Assert.assertEquals("Ali Yerligul",settings.fullName.getAttribute("value"));
+    }
+
+
+    @When("User enters aliyerligulaliyerligulaliyerligul")
+
+    public void aliyerligulaliyerligulaliyerligul() throws InterruptedException {
+        settings.fullName.clear();
+        settings.fullName.sendKeys("aliyerligulaliyerligulaliyerligul");
+        Thread.sleep(8000);
+        Driver.getDriver().navigate().refresh();
+    }
+
+    @Then("User should not see the entered invalid full name")// case of no error it is a bug
+    public void userShouldGetInvalidMessageAsWarning() {
+        String notExpectedFullName = "aliyerligulaliyerligulaliyerligul";
+        String actualFullName=settings.fullName.getAttribute("value");
+        Assert.assertEquals(actualFullName,notExpectedFullName);
     }
 }
