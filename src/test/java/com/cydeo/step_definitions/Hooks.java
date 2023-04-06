744ec2e50e876a3b265d8274fe8c4978b2629019
Albert Shima <albertshima@gmail.com>
2023-04-07 00:04:08 +0200

    Adding files for DATABASE testing

--- src/test/java/com/cydeo/step_definitions/Hooks.java
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -1,10 +1,12 @@
 package com.cydeo.step_definitions;
 
-// In this class we will be able to pass post and pre conditions to each scenario and each step.
+// In this class we will be able to pass post and pre-conditions to each scenario and each step.
 
-import com.cydeo.utilites.BrowserUtils;
+import com.cydeo.utilites.DBUtils;
 import com.cydeo.utilites.Driver;
-import io.cucumber.java.*;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
@@ -13,11 +15,11 @@ public class Hooks {
     // import from io.cucumber.java not from junit
 
     @After
-    public void teardownScenario(Scenario scenario){
+    public void teardownScenario(Scenario scenario) {
 
-        if (scenario.isFailed()){ // If scenario fails, take me the screenshot.
+        if (scenario.isFailed()) { // If scenario fails, take me the screenshot.
             byte[] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
-            scenario.attach(screenshot,"image/png",scenario.getName());
+            scenario.attach(screenshot, "image/png", scenario.getName());
         }
 
         //BrowserUtils.sleep(5);
@@ -27,7 +29,18 @@ public class Hooks {
         //System.out.println("Scenario ended. Take screenshot if failed!");
     }
 
+    @Before("@db")
+    public void setUpDB() {
+        System.out.println("creating database connection...");
+        DBUtils.createConnection();
 
+    }
+
+    @After("@db")
+    public void tearDownDb() {
+        System.out.println("ending database connection...");
+        DBUtils.destroy();
+    }
 
 
 }
